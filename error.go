d6df444604165c2b7fafaf5196c5fd55b8db716c
Max Wolter <awishformore@gmail.com>
2020-05-17 05:12:20 +0200

    added all missing zerolog fields

--- error.go
+++ error.go
@@ -3,7 +3,11 @@ package rich
 import (
 	"errors"
 	"fmt"
+	"net"
+	"runtime"
 	"time"
+
+	"github.com/rs/zerolog"
 )
 
 type Error struct {
@@ -88,16 +92,6 @@ func (e *Error) Float64(key string, val float64) *Error {
 	return e
 }
 
-func (e *Error) Bytes(key string, val []byte) *Error {
-	e.fs = append(e.fs, bytesField{key, val})
-	return e
-}
-
-func (e *Error) Hex(key string, val []byte) *Error {
-	e.fs = append(e.fs, hexField{key, val})
-	return e
-}
-
 func (e *Error) Str(key string, val string) *Error {
 	e.fs = append(e.fs, strField{key, val})
 	return e
@@ -118,12 +112,170 @@ func (e *Error) Time(key string, val time.Time) *Error {
 	return e
 }
 
+func (e *Error) Bools(key string, val []bool) *Error {
+	e.fs = append(e.fs, boolsField{key, val})
+	return e
+}
+
+func (e *Error) Ints(key string, val []int) *Error {
+	e.fs = append(e.fs, intsField{key, val})
+	return e
+}
+
+func (e *Error) Ints8(key string, val []int8) *Error {
+	e.fs = append(e.fs, ints8Field{key, val})
+	return e
+}
+
+func (e *Error) Ints16(key string, val []int16) *Error {
+	e.fs = append(e.fs, ints16Field{key, val})
+	return e
+}
+
+func (e *Error) Ints32(key string, val []int32) *Error {
+	e.fs = append(e.fs, ints32Field{key, val})
+	return e
+}
+
+func (e *Error) Ints64(key string, val []int64) *Error {
+	e.fs = append(e.fs, ints64Field{key, val})
+	return e
+}
+
+func (e *Error) Uints(key string, val []uint) *Error {
+	e.fs = append(e.fs, uintsField{key, val})
+	return e
+}
+
+func (e *Error) Uints8(key string, val []uint8) *Error {
+	e.fs = append(e.fs, uints8Field{key, val})
+	return e
+}
+
+func (e *Error) Uints16(key string, val []uint16) *Error {
+	e.fs = append(e.fs, uints16Field{key, val})
+	return e
+}
+
+func (e *Error) Uints32(key string, val []uint32) *Error {
+	e.fs = append(e.fs, uints32Field{key, val})
+	return e
+}
+
+func (e *Error) Uints64(key string, val []uint64) *Error {
+	e.fs = append(e.fs, uints64Field{key, val})
+	return e
+}
+
+func (e *Error) Floats32(key string, val []float32) *Error {
+	e.fs = append(e.fs, floats32Field{key, val})
+	return e
+}
+
+func (e *Error) Floats64(key string, val []float64) *Error {
+	e.fs = append(e.fs, floats64Field{key, val})
+	return e
+}
+
+func (e *Error) Strs(key string, val []string) *Error {
+	e.fs = append(e.fs, strsField{key, val})
+	return e
+}
+
+func (e *Error) Errs(key string, val []error) *Error {
+	e.fs = append(e.fs, errsField{key, val})
+	return e
+}
+
+func (e *Error) Durs(key string, val []time.Duration) *Error {
+	e.fs = append(e.fs, dursField{key, val})
+	return e
+}
+
+func (e *Error) Times(key string, val []time.Time) *Error {
+	e.fs = append(e.fs, timesField{key, val})
+	return e
+}
+
+func (e *Error) Bytes(key string, val []byte) *Error {
+	e.fs = append(e.fs, bytesField{key, val})
+	return e
+}
+
+func (e *Error) Hex(key string, val []byte) *Error {
+	e.fs = append(e.fs, hexField{key, val})
+	return e
+}
+
 func (e *Error) RawJSON(key string, val []byte) *Error {
 	e.fs = append(e.fs, jsonField{key, val})
 	return e
 }
 
+func (e *Error) IPAddr(key string, val net.IP) *Error {
+	e.fs = append(e.fs, ipField{key, val})
+	return e
+}
+
+func (e *Error) IPPrefix(key string, val net.IPNet) *Error {
+	e.fs = append(e.fs, prefixField{key, val})
+	return e
+}
+
+func (e *Error) MACAddr(key string, val net.HardwareAddr) *Error {
+	e.fs = append(e.fs, macField{key, val})
+	return e
+}
+
 func (e *Error) Interface(key string, val interface{}) *Error {
 	e.fs = append(e.fs, ifaceField{key, val})
+	return e
+}
+
+func (e *Error) Timestamp() *Error {
+	e.fs = append(e.fs, tsField{zerolog.TimestampFunc()})
+	return e
+}
+
+func (e *Error) TimeDiff(key string, val1 time.Time, val2 time.Time) *Error {
+	e.fs = append(e.fs, diffField{key, val1, val2})
+	return e
+}
+
+func (e *Error) Fields(val map[string]interface{}) *Error {
+	e.fs = append(e.fs, fieldsField{val})
+	return e
+}
+
+func (e *Error) Array(key string, val zerolog.LogArrayMarshaler) *Error {
+	e.fs = append(e.fs, arrayField{key, val})
+	return e
+}
+
+func (e *Error) Dict(key string, val *zerolog.Event) *Error {
+	e.fs = append(e.fs, dictField{key, val})
+	return e
+}
+
+func (e *Error) Object(key string, val zerolog.LogObjectMarshaler) *Error {
+	e.fs = append(e.fs, objectField{key, val})
+	return e
+}
+
+func (e *Error) EmbedObject(val zerolog.LogObjectMarshaler) *Error {
+	e.fs = append(e.fs, embedField{val})
+	return e
+}
+
+func (e *Error) Caller(val ...int) *Error {
+	skip := zerolog.CallerSkipFrameCount
+	if len(val) > 0 {
+		skip = val[0] + zerolog.CallerSkipFrameCount
+	}
+	_, file, line, ok := runtime.Caller(skip)
+	if !ok {
+		return e
+	}
+	e.fs = append(e.fs, callerField{file, line})
 	return e
 }
--- field.go
+++ field.go
@@ -2,6 +2,7 @@ package rich
 
 import (
 	"fmt"
+	"net"
 	"strings"
 	"time"
 
@@ -192,32 +193,6 @@ func (f float64Field) Log(ev *zerolog.Event) {
 	ev.Float64(f.key, f.val)
 }
 
-type bytesField struct {
-	key string
-	val []byte
-}
-
-func (f bytesField) String() string {
-	return fmt.Sprintf("%s: %s", f.key, f.val)
-}
-
-func (f bytesField) Log(ev *zerolog.Event) {
-	ev.Bytes(f.key, f.val)
-}
-
-type hexField struct {
-	key string
-	val []byte
-}
-
-func (f hexField) String() string {
-	return fmt.Sprintf("%s: %x", f.key, f.val)
-}
-
-func (f hexField) Log(ev *zerolog.Event) {
-	ev.Hex(f.key, f.val)
-}
-
 type strField struct {
 	key string
 	val string
@@ -263,11 +238,258 @@ type timeField struct {
 }
 
 func (f timeField) String() string {
-	return fmt.Sprintf("%s: %s", f.key, f.val.Format(time.RFC3339))
+	return fmt.Sprintf("%s: %s", f.key, f.val.Format(zerolog.TimeFieldFormat))
 }
 
 func (f timeField) Log(ev *zerolog.Event) {
 	ev.Time(f.key, f.val)
+}
+
+type boolsField struct {
+	key string
+	val []bool
+}
+
+func (f boolsField) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f boolsField) Log(ev *zerolog.Event) {
+	ev.Bools(f.key, f.val)
+}
+
+type intsField struct {
+	key string
+	val []int
+}
+
+func (f intsField) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f intsField) Log(ev *zerolog.Event) {
+	ev.Ints(f.key, f.val)
+}
+
+type ints8Field struct {
+	key string
+	val []int8
+}
+
+func (f ints8Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f ints8Field) Log(ev *zerolog.Event) {
+	ev.Ints8(f.key, f.val)
+}
+
+type ints16Field struct {
+	key string
+	val []int16
+}
+
+func (f ints16Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f ints16Field) Log(ev *zerolog.Event) {
+	ev.Ints16(f.key, f.val)
+}
+
+type ints32Field struct {
+	key string
+	val []int32
+}
+
+func (f ints32Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f ints32Field) Log(ev *zerolog.Event) {
+	ev.Ints32(f.key, f.val)
+}
+
+type ints64Field struct {
+	key string
+	val []int64
+}
+
+func (f ints64Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f ints64Field) Log(ev *zerolog.Event) {
+	ev.Ints64(f.key, f.val)
+}
+
+type uintsField struct {
+	key string
+	val []uint
+}
+
+func (f uintsField) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f uintsField) Log(ev *zerolog.Event) {
+	ev.Uints(f.key, f.val)
+}
+
+type uints8Field struct {
+	key string
+	val []uint8
+}
+
+func (f uints8Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f uints8Field) Log(ev *zerolog.Event) {
+	ev.Uints8(f.key, f.val)
+}
+
+type uints16Field struct {
+	key string
+	val []uint16
+}
+
+func (f uints16Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f uints16Field) Log(ev *zerolog.Event) {
+	ev.Uints16(f.key, f.val)
+}
+
+type uints32Field struct {
+	key string
+	val []uint32
+}
+
+func (f uints32Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f uints32Field) Log(ev *zerolog.Event) {
+	ev.Uints32(f.key, f.val)
+}
+
+type uints64Field struct {
+	key string
+	val []uint64
+}
+
+func (f uints64Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f uints64Field) Log(ev *zerolog.Event) {
+	ev.Uints64(f.key, f.val)
+}
+
+type floats32Field struct {
+	key string
+	val []float32
+}
+
+func (f floats32Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f floats32Field) Log(ev *zerolog.Event) {
+	ev.Floats32(f.key, f.val)
+}
+
+type floats64Field struct {
+	key string
+	val []float64
+}
+
+func (f floats64Field) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f floats64Field) Log(ev *zerolog.Event) {
+	ev.Floats64(f.key, f.val)
+}
+
+type strsField struct {
+	key string
+	val []string
+}
+
+func (f strsField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f strsField) Log(ev *zerolog.Event) {
+	ev.Strs(f.key, f.val)
+}
+
+type errsField struct {
+	key string
+	err []error
+}
+
+func (f errsField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.err)
+}
+
+func (f errsField) Log(ev *zerolog.Event) {
+	ev.Errs(f.key, f.err)
+}
+
+type dursField struct {
+	key string
+	val []time.Duration
+}
+
+func (f dursField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f dursField) Log(ev *zerolog.Event) {
+	ev.Durs(f.key, f.val)
+}
+
+type timesField struct {
+	key string
+	val []time.Time
+}
+
+func (f timesField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f timesField) Log(ev *zerolog.Event) {
+	ev.Times(f.key, f.val)
+}
+
+type bytesField struct {
+	key string
+	val []byte
+}
+
+func (f bytesField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f bytesField) Log(ev *zerolog.Event) {
+	ev.Bytes(f.key, f.val)
+}
+
+type hexField struct {
+	key string
+	val []byte
+}
+
+func (f hexField) String() string {
+	return fmt.Sprintf("%s: %x", f.key, f.val)
+}
+
+func (f hexField) Log(ev *zerolog.Event) {
+	ev.Hex(f.key, f.val)
 }
 
 type jsonField struct {
@@ -283,6 +505,45 @@ func (f jsonField) Log(ev *zerolog.Event) {
 	ev.RawJSON(f.key, f.val)
 }
 
+type ipField struct {
+	key string
+	val net.IP
+}
+
+func (f ipField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f ipField) Log(ev *zerolog.Event) {
+	ev.IPAddr(f.key, f.val)
+}
+
+type prefixField struct {
+	key string
+	val net.IPNet
+}
+
+func (f prefixField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f prefixField) Log(ev *zerolog.Event) {
+	ev.IPPrefix(f.key, f.val)
+}
+
+type macField struct {
+	key string
+	val net.HardwareAddr
+}
+
+func (f macField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f macField) Log(ev *zerolog.Event) {
+	ev.MACAddr(f.key, f.val)
+}
+
 type ifaceField struct {
 	key string
 	val interface{}
@@ -291,7 +552,112 @@ type ifaceField struct {
 func (f ifaceField) String() string {
 	return fmt.Sprintf("%s: %s", f.key, f.val)
 }
-
 func (f ifaceField) Log(ev *zerolog.Event) {
 	ev.Interface(f.key, f.val)
+}
+
+type tsField struct {
+	val time.Time
+}
+
+func (f tsField) String() string {
+	return fmt.Sprintf("%s: %s", zerolog.TimestampFieldName, f.val.Format(zerolog.TimeFieldFormat))
+}
+
+func (f tsField) Log(ev *zerolog.Event) {
+	ev.Time(zerolog.TimestampFieldName, f.val)
+}
+
+type diffField struct {
+	key  string
+	val1 time.Time
+	val2 time.Time
+}
+
+func (f diffField) String() string {
+	return fmt.Sprintf("%s: %s -> %s", f.key, f.val1.Format(zerolog.TimeFieldFormat), f.val2.Format(zerolog.TimeFieldFormat))
+}
+
+func (f diffField) Log(ev *zerolog.Event) {
+	ev.TimeDiff(f.key, f.val1, f.val2)
+}
+
+type fieldsField struct {
+	val map[string]interface{}
+}
+
+func (f fieldsField) String() string {
+	ss := make([]string, 0, len(f.val))
+	for k, v := range f.val {
+		ss = append(ss, fmt.Sprintf("%s: %s", k, v))
+	}
+	return strings.Join(ss, ", ")
+}
+
+func (f fieldsField) Log(ev *zerolog.Event) {
+	ev.Fields(f.val)
+}
+
+type arrayField struct {
+	key string
+	val zerolog.LogArrayMarshaler
+}
+
+func (f arrayField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f arrayField) Log(ev *zerolog.Event) {
+	ev.Array(f.key, f.val)
+}
+
+type dictField struct {
+	key string
+	val *zerolog.Event
+}
+
+func (f dictField) String() string {
+	return fmt.Sprintf("%s: %v", f.key, f.val)
+}
+
+func (f dictField) Log(ev *zerolog.Event) {
+	ev.Dict(f.key, f.val)
+}
+
+type objectField struct {
+	key string
+	val zerolog.LogObjectMarshaler
+}
+
+func (f objectField) String() string {
+	return fmt.Sprintf("%s: %s", f.key, f.val)
+}
+
+func (f objectField) Log(ev *zerolog.Event) {
+	ev.Object(f.key, f.val)
+}
+
+type embedField struct {
+	val zerolog.LogObjectMarshaler
+}
+
+func (f embedField) String() string {
+	return fmt.Sprintf("%s", f.val)
+}
+
+func (f embedField) Log(ev *zerolog.Event) {
+	ev.EmbedObject(f.val)
+}
+
+type callerField struct {
+	file string
+	line int
+}
+
+func (f callerField) String() string {
+	return fmt.Sprintf("%s: %s", zerolog.CallerFieldName, zerolog.CallerMarshalFunc(f.file, f.line))
+}
+
+func (f callerField) Log(ev *zerolog.Event) {
+	ev.Str(zerolog.CallerFieldName, zerolog.CallerMarshalFunc(f.file, f.line))
 }
